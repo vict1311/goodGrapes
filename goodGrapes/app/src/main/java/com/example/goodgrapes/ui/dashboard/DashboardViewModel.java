4ebc6868f5df12d9d63387f863c963c80fdabe60
vict1311 <91823836+vict1311@users.noreply.github.com>
2022-04-07 10:29:38 +0200

    ChangeText
    We changed the text of some fragments!

--- goodGrapes/app/src/main/java/com/example/goodgrapes/ui/dashboard/DashboardViewModel.java
+++ goodGrapes/app/src/main/java/com/example/goodgrapes/ui/dashboard/DashboardViewModel.java
@@ -10,7 +10,7 @@ public class DashboardViewModel extends ViewModel {
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("Good Grapes!");
     }
 
     public LiveData<String> getText() {
